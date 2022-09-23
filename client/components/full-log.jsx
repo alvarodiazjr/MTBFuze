2699b346ef8f642525c191dafebd3599bf54996f
Alvaro Diaz <alvarodiazjr@yahoo.com>
2022-09-23 18:25:35 +0000

    Added icon and basic modal functions.

--- client/components/full-log.jsx
+++ client/components/full-log.jsx
@@ -3,7 +3,12 @@ import React from 'react';
 export default class FullLog extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { rideLog: null };
+    this.state = {
+      rideLog: null,
+      modalOpened: false
+    };
+    this.clickIcon = this.clickIcon.bind(this);
+    this.clickLink = this.clickLink.bind(this);
   }
 
   componentDidMount() {
@@ -20,11 +25,24 @@ export default class FullLog extends React.Component {
       .catch(err => console.error(err));
   }
 
+  clickIcon() {
+    this.setState({ modalOpened: true });
+  }
+
+  clickLink() {
+    this.setState({ modalOpened: false });
+  }
+
   render() {
     if (!this.state.rideLog) return null;
+
     const { photoUrl, location, caption, visitedOn } = this.state.rideLog;
+
     const date = new Date(visitedOn);
     const newDate = new Intl.DateTimeFormat('en-US').format(date);
+
+    const modal = this.state.modalOpened ? 'visible' : 'hidden';
+
     return (
       <div className="container">
         <div className="log-wrapper log-page">
@@ -34,9 +52,27 @@ export default class FullLog extends React.Component {
           <div className='log-text log-content'>
             <h1>{location}</h1>
             <h3>{caption}</h3>
-            <p>{newDate}</p>
+            <div className='row align-center'>
+              <div className='column-half'>
+                <p>{newDate}</p>
+              </div>
+              <div className='column-half text-right'>
+                <i onClick={this.clickIcon} className="fa-solid fa-pencil"></i>
+              </div>
+            </div>
           </div>
         </div>
+        <div className={`log-modal-menu text-center ${modal}`}>
+          <div className='full-log-buttons'>
+            <button onClick={this.clickLink} className='edit-button'>Edit Log</button>
+          </div>
+          <div className='full-log-buttons'>
+            <a href="#delete-modal" className='delete-button' onClick={this.clickLink}>
+              Delete Log
+            </a>
+          </div>
+        </div>
+        <div onClick={this.clickLink} className={`log-modal ${modal}`}></div>
       </div>
     );
   }
