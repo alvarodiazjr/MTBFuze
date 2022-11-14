aa41f3b832054fd9826e4e0791407047965f5447
Alvaro Diaz <alvarodiazjr@yahoo.com>
2022-11-14 23:15:25 +0000

    added login info to data.sql

--- database/data.sql
+++ database/data.sql
@@ -1,2 +1,2 @@
 insert into "users" ("username", "hashedPassword")
-values ('alvarodiaz', 'password1');
+values ('TestUser', '$argon2id$v=19$m=4096,t=3,p=1$e4+lBzuvie1NnJGRbVPjlg$ADFLRoVoxQfz3ZXNQevprOGiO6tSsg+bYz3KTZHV/6g');
